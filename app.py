2bb0b48eb9e3442cef38666985fcfe3053ef2446
Daniel <dwgrossberg@gmail.com>
2024-11-01 12:17:00 -0400

    Change apscheduler time to 60 seconds

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ scheduler = APScheduler()
 
 
 # Schedule a scraper to save internship listing data in csv format
-@scheduler.task('interval', id='scrape_listings', minutes=90,
+@scheduler.task('interval', id='scrape_listings', seconds=60,
                 misfire_grace_time=900)
 def seed_db():
     s = Scraper()
